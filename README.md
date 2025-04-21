5a75099a2d9e4410a785c1c134706bd16d586723
Caden Howell <caden@codepawconsulting.com>
2025-04-20 19:11:32 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,52 @@
+
+# Historical Weather Application
+
+## Requirements
+
+- uv
+- Python 3.11 or above
+- Pandas
+
+## Usage
+
+From the command line, use uv to run the program.  For example, 
+```
+uv run historical_weather.py -h
+```
+
+
+The usage instructions are summed up by the results of running the script with the -h flag.
+The only difference is that it is not necessary to put `function_name` last.  You can put it first as it was done in the project instructions.
+
+```bash
+usage: historical_weather.py [-h] [-c CITY] [-y YEAR] [-m MONTH] [-f FILENAME] [-v] function_name
+
+Perform a calculation on historical weather data: the greatest daily temperature change for a particular city, or
+the average yearly days of precipitation for a particular city.
+
+positional arguments:
+  function_name         Required. Either max-temp-delta or days-of-precip
+
+options:
+  -h, --help            show this help message and exit
+  -c CITY, --city CITY  Required. Which city to evaluate: bos, jnu, or mia
+  -y YEAR, --year YEAR  (max-temp-delta only) Optional. Restrict search to a particular year in the range
+                        2010-2019.
+  -m MONTH, --month MONTH
+                        (max-temp-delta only) Optional. Restrict search to a particular month in the range
+                        1-12.) Requires a year.
+  -f FILENAME, --filename FILENAME
+                        Optional. Filename, defaults to data/noaa_historical_weather_10yr.csv
+  -v, --verbose         Optional. Print more output than just the solution.
+```
+
+## Design choices
+
+- **Modules or one file?**  For such a small script, I decided to use a single file.  As the script grows, I would review the structure and start breaking it into modules.  In this script the code is generally grouped into input handling and calculations, so I would expect the first refactoring to organize the code along those lines.
+- **Type hints?**  I like type hints.  I didn't put them in to cut down on time spent on the assignment.  I also find them helpful when reading large code bases to help me see where data flows in and out of of methods and functions.  For a short script like this where we have a good sense of the data types from the easy to understand numeric concepts (temperature and rainfall instead of, say, n-dimensional matrices of abstract values) I didn't prioritize it.
+- **Rounding?**  I went with 1 decimal, since the input file seems to be to 1 decimal for the values used.
+- **Pandas or just Python?**  I went out on a limb and assumed you use Pandas, and that I should show some basic understanding of it. This arithmetic could have been done with arrays and loops and avoided including the sizable Pandas library.  However, there are a lot of succinct and reusable Pandas features, like the ability to import a CSV file in one line, and support for complicated math. Should this script be expanded, it's likely that those would be helpful.
+- **Testing?**  I tested the code manually and compared the results to calculations performed in LibreOffice Calc.  It needs unit tests!  I would probably generate a first draft with an AI coding tool and update it to test that:
+  - Arguments are parsed correctly
+  - Bad arguments cause the program to exit without blowing up, and give useful feedback
+  - Calculations are correct
---
+++ historical_weather.py
@@ -0,0 +1,140 @@
+import argparse
+import sys
+import pandas as pd
+
+FILENAME = "data/noaa_historical_weather_10yr.csv"
+
+STATION_MAP = {
+    "bos": "USW00014739",
+    "jnu": "USW00025309",
+    "mia": "USW00012839"
+}
+
+def parse_args():
+    """Parse command line arguments."""
+    parser = argparse.ArgumentParser(description="Perform a function on historical weather data: the greatest daily temperature change for a particular city, or the average yearly days of precipitation for a particular city.")
+    parser.add_argument("function_name", help="Required. Either max-temp-delta or days-of-precip")
+    parser.add_argument("-c", "--city", type=str, help="Required. Which city to evaluate: bos, jnu, or mia")
+    parser.add_argument("-y", "--year", type=int, help="(max-temp-delta only) Optional.  Restrict search to a particular year in the range 2010-2019.")
+    parser.add_argument("-m", "--month", type=int, help="(max-temp-delta only) Optional.  Restrict search to a particular month in the range 1-12.)  Requires a year.")
+    parser.add_argument("-f", "--filename", type=str, default=FILENAME, help=f"Optional. Filename, defaults to {FILENAME}")
+    parser.add_argument("-v", "--verbose", action='store_true', help="Optional. Print more output than just the solution.")
+
+    args = parser.parse_args()
+    validate_args(args)
+
+    if args.verbose:
+        print("Verbose output enabled.")
+
+        if args.function_name is not None:
+            print(f"Function name: {args.function_name}")
+        if args.city is not None:
+            print(f"City: {args.city}, maps to {STATION_MAP[args.city]}")
+        if args.year is not None:
+            print(f"Year: {args.year}")
+        if args.month is not None:
+            print(f"Month: {args.month}")
+
+    return args
+
+
+def validate_args(args):
+    valid_args = True
+
+    if args.month is not None:
+        if args.year is None:
+            print("Specifying a month requires a year.")
+            valid_args = False
+        if args.month < 1 or args.month > 12:
+            print("Month must be between 1 and 12.")
+            valid_args = False
+
+    if args.year is not None:
+        if args.year < 2010 or args.year > 2019:
+            print("Year must be between 2010 and 2019.")
+            valid_args = False
+
+    if args.city is not None:
+        if args.city not in ['bos', 'jnu', 'mia']:
+            print("City must be either bos, jnu, or mia.")
+            valid_args = False
+    else:
+        print("City must be specified.")
+        valid_args = False
+
+    if not valid_args:
+        sys.exit(1)
+
+
+def verbose_out(args, message):
+    if args.verbose is True:
+        print(message)
+
+
+def days_of_precip(df, args):
+    # make new combined precipitation row
+    df['SNOW'].fillna(0, inplace=True)
+    df['PRCP'].fillna(0, inplace=True)
+    df["TOTAL_PRECIP"] = df["PRCP"] + df["SNOW"]
+    verbose_out(args,"With total precipitation:")
+    verbose_out(args, df.head())
+    # filter to days with non-zero total precipitation
+    df = df[df["TOTAL_PRECIP"] > 0]
+    # count up by year
+    df = df.groupby(["YEAR"]).count()
+    verbose_out(args,"Grouped by year:")
+    verbose_out(args, df.head())
+    average_days = df["TOTAL_PRECIP"].mean()
+    print(f"days_of_precip: {average_days:.1f}")
+    # We are printing instead of using the result, but normally it seems like a function like this would return it.
+    return average_days
+
+def max_temp_delta(df, args):
+    # filter to year, month
+    if args.year is not None:
+        df = df[df["YEAR"] == args.year]
+    if args.month is not None:
+        df = df[df["MONTH"] == args.month]
+
+    # Make a copy of the DataFrame to avoid SettingWithCopyWarning
+    df = df.copy()
+    # Alternatively, use the Pandas 3 feature which 
+    # pd.options.mode.copy_on_write = True
+
+    # make a new column with temp delta
+    df["TEMP_DELTA"] = df["TMAX"] - df["TMIN"]
+    verbose_out(args,"With temp delta:")
+    verbose_out(args, df.head())
+    max_delta = df["TEMP_DELTA"].max()
+    print(f"max_temp_delta: {max_delta:.1f}")
+    # We are printing instead of using the result, but normally it seems like a function like this would return it.
+    return max_delta
+
+def main():
+  
+    
+    # Process the file and options
+    args =  parse_args()
+    verbose_out(args, "Processing file...")
+
+    df = pd.read_csv(f"{args.filename}")
+    # Drop unused columns
+    df = df[["PRCP","SNOW","TMAX","TMIN","STATION","DATE"]]
+
+
+    # All cases are limited by city
+    station_code = STATION_MAP[args.city]
+    df = df[df["STATION"] == station_code]
+    # We need month and year
+    df["DATE"] = pd.to_datetime(df["DATE"])
+    df["MONTH"] = df["DATE"].dt.month
+    df["YEAR"] = df["DATE"].dt.year
+    verbose_out(args,"Data sample:")
+    verbose_out(args, df.head())
+    if args.function_name == "days-of-precip":
+        days_of_precip(df, args)
+    if args.function_name == "max-temp-delta":
+        max_temp_delta(df, args)
+
+if __name__ == "__main__":
+    main()
